4cb04b2d2f21908a1de790100e8149336b02be4b
Abhishek-Hiremath49 <abhishekhiremath4949@gmail.com>
2026-03-02 09:31:20 +0000

    added: youtube analytics scopes

--- frappe_social/frappe_social/api/oauth.py
+++ frappe_social/frappe_social/api/oauth.py
@@ -128,7 +128,7 @@ def _get_auth_url(platform: str, settings, redirect_uri: str, state: str) -> str
             "https://www.googleapis.com/auth/userinfo.email",
             "https://www.googleapis.com/auth/youtube.upload",
             "https://www.googleapis.com/auth/youtube.readonly",
-            "https://www.googleapis.com/auth/yt.analytics.readonly",
+            "https://www.googleapis.com/auth/yt-analytics.readonly",
             # "https://www.googleapis.com/auth/youtube",
             # "https://www.googleapis.com/auth/youtube.force-ssl",
         ])
